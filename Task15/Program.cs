52c54feb0a42730bc166609b3591b0816579f541
Ayra <777aira777@gmail.com>
2022-10-08 21:02:59 +0400

    Добавлено другое решение 15 задачи

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -35,19 +35,30 @@
 // break;
 // }
 
+// Console.WriteLine("Введите день недели от 1 до 7:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// while (number < 1 | number > 7) 
+// {
+// Console.WriteLine("В неделе 7 дней, введите номер дня недели от 1 до 7:");
+// number = Convert.ToInt32(Console.ReadLine());
+// }
+// if (number == 6| number == 7) Console.Write("да");
+// else Console.WriteLine("нет");
 
-Console.WriteLine("Введите номер дня недели: ");
+
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number < 1 || number > 7)
-    Console.WriteLine("Нет такого дня недели!");
-else
+while (number < 1 | number > 7)
 {
-    if (dayOff(number))
-        Console.WriteLine("да");
-    else Console.WriteLine("нет");
-
+    Console.WriteLine("В неделе 7 дней, введите номер дня недели от 1 до 7:");
+    number = Convert.ToInt32(Console.ReadLine());
 }
+
+if (dayOff(number))
+    Console.WriteLine("да");
+else Console.WriteLine("нет");
+
 bool dayOff(int num)
 {
     return num == 6 || num == 7;
